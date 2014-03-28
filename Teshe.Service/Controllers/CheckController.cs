008d936f09d5b8bcb7f36fea8206d8edf1ccf273
happybai <happybai1991@gmail.com>
2014-03-28 09:33:56 +0800

    Android客户端添加验证

---
+++ Teshe.Service/Controllers/CheckController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Teshe.Models;
+
+namespace Teshe.Service.Controllers
+{
+    public class CheckController : BaseController
+    {
+        public bool Check(String username, String password, String simcode)
+        {
+            UserInfo user = db.UserInfoes.FirstOrDefault<UserInfo>(u => u.Name == username && u.Password == password && u.SIMCode == simcode);
+            if (user != null)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
